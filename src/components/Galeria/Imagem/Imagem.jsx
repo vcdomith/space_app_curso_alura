a655e22ef8ec87986d550ca9323b46c4c450f2bd
Vitor Domith <vcdomith@hotmail.com>
2023-12-18 19:04:06 -0300

    Styled Component Changes

--- src/components/Galeria/Imagem/Imagem.jsx
+++ src/components/Galeria/Imagem/Imagem.jsx
@@ -1,32 +1,47 @@
 import {styled} from 'styled-components'
 
 const FigureContainer = styled.figure`
-    min-width: 448px;
-    min-height: 336px;
+    width: 448px;
+    height: 336px;
 
     margin: 0;
 
     border-radius: 1rem;
+    overflow: hidden;
+
+    background-color: #001634;
 `
 
 const ImageStyle = styled.img`
     /* aspect-ratio: 7 / 4; */
     width: 28rem;
-    height: 16rem;
+    max-height: 240px;
+`
+
+const FigureCaptionContainerStyle = styled.figcaption`
+    height: 96px;
+`
+
+const TitleStyle = styled.h3`
+    margin: 0;
+`
+
+const SourceStyle = styled.p`
+    margin: 0;
 `
 
 const Imagem = ({ foto }) => {
   return (
     <FigureContainer>
         <ImageStyle src={foto.path} alt={`Foto de ${foto.titulo}`} />
-        <figcaption>
-            <h3>{foto.titulo}</h3>
+        <FigureCaptionContainerStyle>
+            <TitleStyle>{foto.titulo}</TitleStyle>
             <footer>
-                <p>{foto.fonte}</p>
+                <SourceStyle>{foto.fonte}</SourceStyle>
                 <button>Favorito</button>
                 <button>Expandir</button>
             </footer>
-        </figcaption>
+        </FigureCaptionContainerStyle>
     </FigureContainer>
   )
 }
